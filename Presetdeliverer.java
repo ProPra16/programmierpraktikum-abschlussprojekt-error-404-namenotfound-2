7d7a4db8bcaa54e1ea41b3e98ff2df2b4c021280
Julian Biston <Julian Biston>
2016-07-01 15:55:01 +0200

    codesegment wird aus presetcode.txt geladen -Julian,-Michael,-Jonas

--- Presetdeliverer.java
+++ Presetdeliverer.java
@@ -1,4 +1,6 @@
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -9,13 +11,13 @@ public class Presetdeliverer {
         static String classname;
         static String classcomment;
         static String testname;
-        public static void main (String [] args){
-            //File presettingcode = new File("presetcode.java");
-            //File presettingtest = new File ("presettest.java");
-            String vorlagecode = presetcode();
-            String vorlagetest = "import static org.junit.Assert.*;\n"+
+        public static void main (){
+        	
+           
+                    
+            String vorlagetest ="import static org.junit.Assert.*;\n"+
                                 "import org.junit.Test;\n"+
-                                "public class §{\n"+
+                                "public class �{\n"+
                                 "   @Test\n" +
                                 "   public void testSomething(){\n" +
                                 "   }\n"+
@@ -23,66 +25,25 @@ public class Presetdeliverer {
             //classname,classcomment,testname,babysteps und timetracker Einstellungen
             //Werden eingetragen bzw. eingelesen.
             XMLReader.reader();
-
-            File presettingcode = new File(PresetDataBase.codeclassname+".java");
-            File presettingtest = new File (PresetDataBase.testclassname+".java");
-            //Die Lücken der CodeStrings werden gefüllt.
-            vorlagecode = fillvorlagecode(vorlagecode);
+            TxtToJava.transform();
+         //   File presettingcode = new File(PresetDataBase.codeclassname+".java");
+            File presettingtest = new File (PresetDataBase.testclassname+".java");          
             vorlagetest = fillvorlagetest(vorlagetest);
-            writecodefile(presettingcode,vorlagecode);
             writetestfile(presettingtest,vorlagetest);
         }
-        public static String fillvorlagecode (String vorlage){
-            //Einfügen des Kommentars
-            vorlage = fillinclasscomment(vorlage);
-            //Einfügen des Klassennamens
-            vorlage = fillinclassname(vorlage);
-            return vorlage;
-        }
-        public static String fillinclasscomment(String vorlage){
-            int i=0;
-            String start = "";
-            String end ="";
-            while(vorlage.charAt(i)!='$'){
-                i++;
-            }
-            start = vorlage.substring(0,i)+classcomment;
-            end = vorlage.substring(i+1,vorlage.length());
-            return start+end;
-        }
-        public static String fillinclassname (String vorlage) {
-            String start ="";
-            String end ="";
-            int i=0;
-            while(vorlage.charAt(i) != '?'){
-                i++;
-            }
-            start = vorlage.substring(0,i)+classname;
-            end = vorlage.substring(i+1,vorlage.length());
-            return start+end;
-        }
+ 
         public static String fillvorlagetest (String testvorlage){
             String start="";
             String end="";
             int i =0;
-            while(testvorlage.charAt(i)!='§'){
+            while(testvorlage.charAt(i)!='�'){
                 i++;
             }
             start = testvorlage.substring(0,i)+testname;
             end = testvorlage.substring(i+1,testvorlage.length());
             return start+end;
         }
-        public static void writecodefile(File outputfile,String code) {
-            Path p = Paths.get(outputfile.getAbsolutePath());
-            try {
-                FileWriter writer = new FileWriter(outputfile);
-                writer.write(code);
-                writer.flush();
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+ 
         public static void writetestfile(File outputfile,String test) {
             Path p = Paths.get(outputfile.getAbsolutePath());
             try {
@@ -93,29 +54,5 @@ public class Presetdeliverer {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-        public static String presetcode (){
-            File code = new File("presetcode.txt");
-            Path p = Paths.get(code.getAbsolutePath());
-            boolean filenotendet=true;
-            String currentreadingline="";
-            String output ="";
-            try {
-                FileReader reader = new FileReader(code);
-                BufferedReader buffer = new BufferedReader(reader);
-                while (filenotendet) {
-                    currentreadingline = buffer.readLine();
-                    if(currentreadingline !=null){
-                        output+=currentreadingline+"\n";
-                    }
-                    if(currentreadingline==null) filenotendet=false;
-
-                }
-            } catch(FileNotFoundException e){
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return output;
         }
     }
---
+++ TxtToJava.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class TxtToJava {
+
+	    
+
+	    public static void transform() {
+	        File source = new File("presetcode.txt");
+	        Path pfad = Paths.get(source.getAbsolutePath());
+
+	        ArrayList<String> buffer = new ArrayList<String>();
+	        try {
+	            buffer = (ArrayList<String>) Files.readAllLines(pfad);
+	        }
+	        catch (IOException e){}
+	        String code = "";
+	        for(int i = 0; i<buffer.size();i++){
+	            if (i==0) code = code + buffer.get(i);
+
+	            else code = code + "\n" + buffer.get(i);
+	        }
+	        save(code);
+	    }
+	    public static void save(String a){
+	        File tmp = new File(PresetDataBase.codeclassname+".java");
+	        Path p = Paths.get(tmp.getAbsolutePath());
+
+	        try {
+	            FileWriter writer = new FileWriter(tmp);
+	            writer.write(a);
+	            writer.flush();
+	            writer.close();
+	        }
+	        catch(IOException e){}
+
+	    }
+}
