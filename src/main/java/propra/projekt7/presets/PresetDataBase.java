2bc7d51a2b6b7a42d2c5cb1ae8d326f26becb8e4
martynovsky <Yulian.Martynovsky@uni-duesseldorf.de>
2016-07-06 23:57:27 +0200

    Revert "Variablen sind jetzt wirklich richtig deklariert."
    This reverts commit 2629e89389e5da6222ec6b9653e84cc1b6544b02.

--- src/main/java/propra/projekt7/presets/PresetDataBase.java
+++ src/main/java/propra/projekt7/presets/PresetDataBase.java
@@ -3,13 +3,13 @@ package propra.projekt7.presets;
  * Created by jonas on 22.06.2016.
  */
 public class PresetDataBase {
-    public static boolean babysteps;
-    public static int babystepstime =0;
+    static boolean babysteps;
+    static int babystepstime =0;
     public static boolean atdd;
     static String codeclassname;
     static String testclassname;
-	public static String codepreset;
-	public static String xmlpreset;
+	static String codepreset;
+	static String xmlpreset;
 	static int ATDDFailedTests;
 	
 }
