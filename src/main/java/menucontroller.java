366aed3c0b4108edb5a6dfd40321e997cb43dc54
martynovsky <Yulian.Martynovsky@uni-duesseldorf.de>
2016-07-27 20:13:34 +0200

    deleted Alert import

--- src/main/java/menucontroller.java
+++ src/main/java/menucontroller.java
@@ -8,7 +8,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
