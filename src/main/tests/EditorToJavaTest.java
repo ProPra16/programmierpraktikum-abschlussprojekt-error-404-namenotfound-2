86642269119c0577011bc897d99218c1e6db39e7
martynovsky <Yulian.Martynovsky@uni-duesseldorf.de>
2016-07-27 18:40:09 +0200

    Revert "wrote ParamTests for EditorToJava and a new test for JavaToEditor"
    This reverts commit 2b87c290180cf4bd6c818afb4c609bd6cbd5b7c1.

--- src/main/tests/EditorToJavaTest.java
+++ src/main/tests/EditorToJavaTest.java
@@ -2,42 +2,11 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-import java.util.Arrays;
-
 /**
- * Created by Martin and Yulian
+ * Created by Martin on 11.07.2016.
  */
-@RunWith(Parameterized.class)
 public class EditorToJavaTest {
-	@parameters
-	public static Collection<String[]> data(){
-		String[] data = {"public class HelloWorld{\n" +
-                "\tpublic static void main(String[] args){\n" +
-                "\t\tSystem.out.println(\"Hello World\");\n" +
-                "\t}\n" +
-                "}", 
-                "public class javaToEditorTestText{\n" +
-                "\tpublic static void main(String[] args){\n" +
-                "\t\ttestText();\n" +
-                "\t}\n" +
-                "\tpublic static void textTest{\n" +
-                "\t\tfor(int i = 0; i < 10; i++){\n" +
-                "\t\t\tif(true){\n" + 
-                "\t\t\t\treturn\n" + 
-                "\t\t\t}\n" +
-                "\t\t}\n" +
-                "\t}\n" +
-                "}"}
-				return Arrays.asList(data);
-		}
-	public EditorToJavaTest(String r){
-		this.r = r;
-	}
-		
-	
-	
-	
-    /*@Test
+    @Test
     public void beschreibtDateiRichtig(){
         String input = "public class HelloWorld{\n" +
                 "\tpublic static void main(String[] args){\n" +
@@ -48,15 +17,6 @@ public class EditorToJavaTest {
         etj.save(input);
         String output = new JavaToEditor("HelloWorld2").read();
         assertEquals(input,output);
-    }*/
-    
-    @Test
-    public void beschreibtDateiRichtig(){
-    	EditorToJava testFile = new EditorToJava(HelloTest);
-    	test.save(r);
-    	String testOutput = new JavaToEditor("HelloTest").read();
-    	assertEquals(r, testOutput);
     }
-    
 
 }
